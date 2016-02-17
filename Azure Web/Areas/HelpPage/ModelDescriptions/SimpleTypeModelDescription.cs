79872585d777afbece545c2d90b125fc9d2f3aa2
Girish Murthy <gpmurthy@gmail.com>
2016-02-16 20:25:24 -0500

    Testinf Azure

---
+++ Azure Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Azure_Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Azure Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Azure_Web.MvcApplication" Language="C#" %>
